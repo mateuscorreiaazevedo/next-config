c5cea617dc72ff0be0efc6a8e47ffd66d9317dad
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2022-10-26 14:48:03 -0300

    initial conig app

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>HomePage</title>
+      </Head>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
